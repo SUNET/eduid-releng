167939017e39f2b0f2e77c88dcf7e10865e4689f
Johan Lundberg <lundberg@sunet.se>
2024-08-21 14:57:02 +0200

    add revision docs and print them on start

--- html/start-html.sh
+++ html/start-html.sh
@@ -4,6 +4,13 @@ set -e
 
 chgrp www-data /var/log/nginx
 
+# nice to have in docker run output, to check what
+# version of something is actually running.
+test -f /revision-html.txt && printf "eduid-html\n" && cat /revision-html.txt; true
+test -f /revision-front.txt && printf "eduid-front\n" && cat /revision-front.txt; true
+test -f /revision-managed-accounts.txt && printf "eduid-managed-accounts\n" && cat /revision-managed-accounts.txt; true
+test -f /submodules.txt && cat /submodules.txt; true
+
 exec start-stop-daemon --start --exec \
      /usr/sbin/nginx \
      --pidfile "/var/run/nginx.pid" \
